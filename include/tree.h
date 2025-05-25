0231a93e3169775a2ca8e3e09cd61a67009b3653
Анна <sol0dova.anna@yandex.ru>
2025-05-25 19:38:30 +0300

    ads-9

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,34 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+
+class TreeNode {
+public:
+    char value;
+    std::vector<TreeNode*> children;
+
+    TreeNode(char val);
+    ~TreeNode();
+
+
+};
+
+class PMTree {
+private:
+    TreeNode* root;
+
+    void build(TreeNode* node, std::vector<char> remaining);
+    void collect(TreeNode* node, std::vector<char>& path, std::vector<std::vector<char>>& result);
+    std::vector<char> getPermByIndex(TreeNode* node, int& index, int target);
+
+public:
+    PMTree(const std::vector<char>& in);
+    ~PMTree();
+
+    friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+    friend std::vector<char> getPerm1(PMTree& tree, int num);
+    friend std::vector<char> getPerm2(PMTree& tree, int num);
+};
+
 #endif  // INCLUDE_TREE_H_
