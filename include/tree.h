5b9dcc4076da92f289829e06ac0e2d40eea3672d
Анна <sol0dova.anna@yandex.ru>
2025-05-25 20:40:16 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -3,31 +3,30 @@
 #define INCLUDE_TREE_H_
 
 #include <vector>
+using namespace std;
 
 class TreeNode {
  public:
     char value;
     std::vector<TreeNode*> children;
 
-    TreeNode(char val);
+    explicit TreeNode(char val);
     ~TreeNode();
-
-
 };
 
 class PMTree {
  private:
     TreeNode* root;
 
-    void build(TreeNode* node, std::vector<char> remaining);
-    void collect(TreeNode* node, std::vector<char>& path, std::vector<std::vector<char>>& result);
+    void build(TreeNode* node, vector<char> remaining);
+    void collect(TreeNode* node, vector<char>& path, vector<std::vector<char>>& result);
     std::vector<char> getPermByIndex(TreeNode* node, int& index, int target);
 
  public:
-    PMTree(const std::vector<char>& in);
+    explicit PMTree(const std::vector<char>& in);
     ~PMTree();
 
-    friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+    friend std::vector<vector<char>> getAllPerms(PMTree& tree);
     friend std::vector<char> getPerm1(PMTree& tree, int num);
     friend std::vector<char> getPerm2(PMTree& tree, int num);
 };
